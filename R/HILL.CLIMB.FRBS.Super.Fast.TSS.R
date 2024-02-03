06dc557f679c2124583fa915c49801fcdb27f149
Rafael M. Mas <rafa.m.mas@gmail.com>
2024-02-03 19:47:19 +0100

    Initial commit

--- R/HILL.CLIMB.FRBS.Super.Fast.TSS.R
+++ R/HILL.CLIMB.FRBS.Super.Fast.TSS.R
@@ -8,56 +8,52 @@
 #' @export
 #'
 #' @examples
-HILL.CLIMB.FRBS.Super.Fast.TSS <- function(Membership, Training.dataset, Trials)
-{
-Optimal.Consequents.Matrix <- NULL
-TSSs <- NULL
+HILL.CLIMB.FRBS.Super.Fast.TSS <- function(Membership, Training.dataset, Trials) {
+  Optimal.Consequents.Matrix <- NULL
+  TSSs <- NULL
 
-for(Trial in 1:Trials)
-{
+  for (Trial in 1:Trials)
+  {
+    set.seed(Trial)
+    Temporal.Consequents <- c(sample(c(0, 1), nrow(Membership), replace = T))
 
-set.seed(Trial)
-Temporal.Consequents <- c(sample(c(0,1), nrow(Membership), replace=T))
-
-Output.value <- apply(Membership, 2, function(x){
-sum(c(x)*c(Temporal.Consequents))/sum(x)
-})
+    Output.value <- apply(Membership, 2, function(x) {
+      sum(c(x) * c(Temporal.Consequents)) / sum(x)
+    })
 
 
-TSS <- sum(caret::confusionMatrix(data=cut(Output.value, breaks=c(0, 0.5 ,1), labels=c(0, 1), include.lowest=T), reference=factor(Training.dataset[,"Species"], levels = c("0", "1")), positive = "1")$byClass[1:2])-1
+    TSS <- sum(caret::confusionMatrix(data = cut(Output.value, breaks = c(0, 0.5, 1), labels = c(0, 1), include.lowest = T), reference = factor(Training.dataset[, "Species"], levels = c("0", "1")), positive = "1")$byClass[1:2]) - 1
 
-TSSRef <- TSS
+    TSSRef <- TSS
 
-set.seed(Trial)
-RandomHillClimbing <- sample(c(1:nrow(Membership)))
+    set.seed(Trial)
+    RandomHillClimbing <- sample(c(1:nrow(Membership)))
 
-for(ii in 1:nrow(Membership))
-{
+    for (ii in 1:nrow(Membership))
+    {
+      Temporal.Consequents[which(RandomHillClimbing == ii)] <- c(0, 1)[which(c(0, 1) != Temporal.Consequents[which(RandomHillClimbing == ii)])]
 
-Temporal.Consequents[which(RandomHillClimbing==ii)] <- c(0,1)[which(c(0,1)!=Temporal.Consequents[which(RandomHillClimbing==ii)])]
+      Output.value <- apply(Membership, 2, function(x) {
+        sum(c(x) * c(Temporal.Consequents)) / sum(x)
+      })
 
-Output.value <- apply(Membership, 2, function(x){
-sum(c(x)*c(Temporal.Consequents))/sum(x)
-})
+      TSS <- sum(caret::confusionMatrix(data = cut(Output.value, breaks = c(0, 0.5, 1), labels = c(0, 1), include.lowest = T), reference = factor(Training.dataset[, "Species"], levels = c("0", "1")), positive = "1")$byClass[1:2]) - 1
 
-TSS <- sum(caret::confusionMatrix(data=cut(Output.value, breaks=c(0, 0.5 ,1), labels=c(0, 1), include.lowest=T), reference=factor(Training.dataset[,"Species"], levels = c("0", "1")), positive = "1")$byClass[1:2])-1
+      if (TSSRef > TSS) {
+        Temporal.Consequents[which(RandomHillClimbing == ii)] <- c(0, 1)[which(c(0, 1) != Temporal.Consequents[which(RandomHillClimbing == ii)])]
+      }
 
-if(TSSRef > TSS)
-{
-Temporal.Consequents[which(RandomHillClimbing==ii)] <- c(0,1)[which(c(0,1)!=Temporal.Consequents[which(RandomHillClimbing==ii)])]
-}
+      if (TSSRef < TSS) {
+        TSSRef <- TSS
+      }
+    }
 
-if(TSSRef < TSS)
-{
-TSSRef <- TSS
-}
-}
+    Optimal.Consequents.Matrix <- cbind(Optimal.Consequents.Matrix, Temporal.Consequents)
+    TSSs <- c(TSSs, TSSRef)
+  }
 
-Optimal.Consequents.Matrix <- cbind(Optimal.Consequents.Matrix,Temporal.Consequents)
-TSSs <- c(TSSs, TSSRef)
-}
-
-Output.value <- apply(Membership, 2, function(x){sum(c(x)*c(Optimal.Consequents.Matrix[,which.max(TSSs)]))/sum(x)})
-return(list(TSS=max(TSSs), Consequents=c(Optimal.Consequents.Matrix[,which.max(TSSs)])))
-
+  Output.value <- apply(Membership, 2, function(x) {
+    sum(c(x) * c(Optimal.Consequents.Matrix[, which.max(TSSs)])) / sum(x)
+  })
+  return(list(TSS = max(TSSs), Consequents = c(Optimal.Consequents.Matrix[, which.max(TSSs)])))
 }
--- R/PREDICT.FRBS.Fast.R
+++ R/PREDICT.FRBS.Fast.R
@@ -7,27 +7,24 @@
 #' @export
 #'
 #' @examples
-PREDICT.FRBS.Fast <- function(Data, FRBS)
-{
+PREDICT.FRBS.Fast <- function(Data, FRBS) {
+  Data <- sapply(colnames(FRBS$Range), function(i) {
+    Data[Data[, i] > FRBS$Range[2, i], i] <- FRBS$Range[2, i]
+    return(Data[, i])
+  })
 
-Data <- sapply(colnames(FRBS$Range), function(i){
-Data[Data[,i]>FRBS$Range[2,i],i] <- FRBS$Range[2,i]
-return(Data[,i])
-})
-
-apply(Data,1,function(x){
-ExpandList <- NULL
-for(ImpVariables in FRBS$ImpVariables)
-{
-ExpandList[[length(ExpandList)+1]] <- apply(FRBS$MFparameters[,stringr::str_detect(string=colnames(FRBS$MFparameters), pattern=ImpVariables)],2,function(parameters)
-{
-FRBS$MFfunction(pattern=x[ImpVariables], parameters=parameters)
-})
-}
-Memb. <- sum(apply(expand.grid(ExpandList),1,prod),na.rm=T)
-Value <- sum(apply(expand.grid(ExpandList),1,prod)*FRBS$Consequents,na.rm=T)/sum(apply(expand.grid(ExpandList),1,prod),na.rm=T)
-return(cbind(round(Memb., 3), round(Value, 3)))
-})
+  apply(Data, 1, function(x) {
+    ExpandList <- NULL
+    for (ImpVariables in FRBS$ImpVariables)
+    {
+      ExpandList[[length(ExpandList) + 1]] <- apply(FRBS$MFparameters[, stringr::str_detect(string = colnames(FRBS$MFparameters), pattern = ImpVariables)], 2, function(parameters) {
+        FRBS$MFfunction(pattern = x[ImpVariables], parameters = parameters)
+      })
+    }
+    Memb. <- sum(apply(expand.grid(ExpandList), 1, prod), na.rm = T)
+    Value <- sum(apply(expand.grid(ExpandList), 1, prod) * FRBS$Consequents, na.rm = T) / sum(apply(expand.grid(ExpandList), 1, prod), na.rm = T)
+    return(cbind(round(Memb., 3), round(Value, 3)))
+  })
 }
 
 #' Function to obtain the membership of each datum to each fuzzy rule
@@ -39,18 +36,16 @@ return(cbind(round(Memb., 3), round(Value, 3)))
 #' @export
 #'
 #' @examples
-FUZZIFY.FRBS.Fast <- function(Data, FRBS)
-{
-apply(Data,1,function(x){
-ExpandList <- NULL
-for(ImpVariables in FRBS$ImpVariables)
-{
-ExpandList[[length(ExpandList)+1]] <- apply(FRBS$MFparameters[,stringr::str_detect(string=colnames(FRBS$MFparameters), pattern=ImpVariables)],2,function(parameters)
-{
-FRBS$MFfunction(pattern=x[ImpVariables], parameters=parameters)
-})
-}
-Memb. <- apply(expand.grid(ExpandList),1,prod)
-return(Memb.)
-})
+FUZZIFY.FRBS.Fast <- function(Data, FRBS) {
+  apply(Data, 1, function(x) {
+    ExpandList <- NULL
+    for (ImpVariables in FRBS$ImpVariables)
+    {
+      ExpandList[[length(ExpandList) + 1]] <- apply(FRBS$MFparameters[, stringr::str_detect(string = colnames(FRBS$MFparameters), pattern = ImpVariables)], 2, function(parameters) {
+        FRBS$MFfunction(pattern = x[ImpVariables], parameters = parameters)
+      })
+    }
+    Memb. <- apply(expand.grid(ExpandList), 1, prod)
+    return(Memb.)
+  })
 }
